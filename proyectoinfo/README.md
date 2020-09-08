55a3af408c217a6f125765da8296501a0638dd1a
Juan Fernandez <jmf798@gmail.com>
2020-09-08 15:52:50 -0300

    primer commit plasmeroarreglo de estructura de carpetas

---
+++ proyectoinfo/README.md
@@ -0,0 +1,2 @@
+# plasma-info
+Repositorio de del proyecto "Donantes de plasma convaleciente" del curso de programación web el Informatorio.
---
+++ proyectoinfo/proyectoinfo/static/js/custom.js
@@ -0,0 +1,6 @@
+function showVal(newVal){
+  document.getElementById("valBox").innerHTML=newVal;
+}
+function reLoad() {
+    location.reload(true);
+}
