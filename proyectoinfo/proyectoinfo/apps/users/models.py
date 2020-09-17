4635a02b61a67cea7cd65d401c4f0470668a2221
facu1 <fac.gonzalez@outlook.com>
2020-09-17 20:06:28 -0300

    commit de modelos

--- proyectoinfo/proyectoinfo/apps/users/models.py
+++ proyectoinfo/proyectoinfo/apps/users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class CentroHabilitado(models.Model):
+    nombre = models.CharField(max_length=50)
+    mail = models.CharField(max_length=50)
+    nro_tel = models.IntegerField(max_length=50)
+    direccion = models.CharField(max_length=50)
+    horario_aten = models.CharField(max_length=50)
+
+    class Meta:
+	    verbose_name = ("CentroHabilitado")
+	    verbose_name_plural = ("CentrosHabilitados")
+
+class Post(models.Model):
+    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    contenido = models.TextField()
+	fecha_creacion = models.DateTimeField(default=timezone.now)
+
+    def publicar(self):
+		self.fecha_publicacion = timezone.now()
+		self.save()
+
+    class Meta:
+	    verbose_name = ("Post")
+	    verbose_name_plural = ("Posts")
