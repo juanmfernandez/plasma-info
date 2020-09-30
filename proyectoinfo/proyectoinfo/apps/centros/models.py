a730869a2020f85d3ad79076db15835d44afb2ab
Juan Fernandez <jmf798@gmail.com>
2020-09-30 20:06:48 -0300

    correcion de estilo para movil en form create_center

--- proyectoinfo/proyectoinfo/apps/centros/models.py
+++ proyectoinfo/proyectoinfo/apps/centros/models.py
@@ -4,9 +4,9 @@ from location_field.models.plain import PlainLocationField
 
 
 class EnableCenter(models.Model):
-    name = models.CharField(max_length=200)
-    mail = models.CharField(max_length=200)
-    phone_num = models.IntegerField()
-    address = models.CharField(max_length=200)
-    location = PlainLocationField(based_fields=['city'], zoom=7)
-    business_hours = models.CharField(max_length=200)
+    name = models.CharField(verbose_name="Nombre del centro", max_length=200)
+    mail = models.CharField(verbose_name="E-mail", max_length=200)
+    phone_num = models.IntegerField(verbose_name="Telefono")
+    address = models.CharField(verbose_name="Dirección", max_length=200)
+    location = PlainLocationField(verbose_name="Ubicación en mapa", based_fields=['city'], zoom=7)
+    business_hours = models.CharField(verbose_name="Horario de atención", max_length=200)
