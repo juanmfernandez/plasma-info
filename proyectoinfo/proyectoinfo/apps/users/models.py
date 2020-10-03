a4971431c811385d5ee879ef7d90d283057aa34f
Juan Fernandez <jmf798@gmail.com>
2020-10-03 00:06:51 -0300

    set unique para DNI y email en model de usuario

--- proyectoinfo/proyectoinfo/apps/users/models.py
+++ proyectoinfo/proyectoinfo/apps/users/models.py
@@ -6,7 +6,8 @@ from django import forms
 class Usuario(AbstractUser, models.Model):
     first_name = models.CharField(verbose_name="Nombres", max_length=50, blank=False)
     last_name = models.CharField(verbose_name="Apellidos", max_length=50, blank=False)
-    dni =  models.CharField(verbose_name="DNI", max_length=9, null=True, blank=True)
+    dni =  models.CharField(verbose_name="DNI", max_length=9, null=True, blank=True, unique=True)
+    email =  models.EmailField(unique=True)
     fecha_nac = models.DateField(verbose_name="Fecha nacimiento", null=True, blank=True)
     grupo_sanguineo = models.CharField(verbose_name="Grupo", max_length=3, null=True, blank=True)
     factor_rh = models.CharField(verbose_name="Factor Rh", max_length=2, null=True, blank=True)
